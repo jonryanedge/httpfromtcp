60b2ef058e5d8ce2a2290523e403ac63456ed29c
Jon Edgeworth <jon@echofive.net>
2025-11-30 09:15:49 -0500

    wip->ch2:l2

---
+++ cmd/tcplistener/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		fmt.Printf("error: %s", err)
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("could not accept a connection")
+		}
+		fmt.Println("connection accepted")
+		go func(c net.Conn) {
+			defer c.Close()
+			lines := getLinesChannel(c)
+			for line := range lines {
+				fmt.Printf("read: %s\n", line)
+			}
+			fmt.Println("connection closed")
+		}(conn)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		chunkSize := 8
+		var currentLine string
+
+		for {
+			buffer := make([]byte, chunkSize) // Create a byte slice of size 8
+			bytesRead, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					ch <- currentLine
+				}
+				if errors.Is(err, io.EOF) {
+					break // Exit the loop at the end of the file
+				}
+				fmt.Printf("error: %s\n", err)
+				break
+			}
+			str := string(buffer[:bytesRead])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				line := currentLine + parts[i]
+				ch <- line
+				currentLine = ""
+			}
+
+			currentLine += parts[len(parts)-1]
+		}
+	}()
+
+	return ch
+}
--- helpers.go
+++ helpers.go
@@ -1,46 +1,8 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"strings"
+// "errors"
+// "fmt"
+// "io"
+// "strings"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(ch)
-
-		chunkSize := 8
-		var currentLine string
-
-		for {
-			buffer := make([]byte, chunkSize) // Create a byte slice of size 8
-			bytesRead, err := f.Read(buffer)
-			if err != nil {
-				if currentLine != "" {
-					ch <- currentLine
-				}
-				if errors.Is(err, io.EOF) {
-					break // Exit the loop at the end of the file
-				}
-				fmt.Printf("error: %s\n", err)
-				break
-			}
-			str := string(buffer[:bytesRead])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				line := currentLine + parts[i]
-				ch <- line
-				currentLine = ""
-			}
-
-			currentLine += parts[len(parts)-1]
-		}
-	}()
-
-	return ch
-}
