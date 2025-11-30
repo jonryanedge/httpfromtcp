63fa354c1fa554ad25811965604d45c9daef8546
Jon Edgeworth <jon@echofive.net>
2025-11-29 20:31:10 -0500

    ch1:done

--- main.go
+++ main.go
@@ -2,38 +2,21 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
+const fileName = "messages.txt" // Replace with your file name
+
 func main() {
-	fileName := "messages.txt" // Replace with your file name
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close() // Ensure the file is closed
 
-	chunkSize := 8
-	buffer := make([]byte, chunkSize) // Create a byte slice of size 8
+	lines := getLinesChannel(file)
 
-	for {
-		// Read up to 8 bytes into the buffer
-		bytesRead, err := file.Read(buffer)
-
-		if err != nil {
-			// Check for end of file, which is normal termination
-			if err == io.EOF {
-				break // Exit the loop at the end of the file
-			}
-			// Handle other potential errors
-			log.Fatal(err)
-		}
-
-		// Process the bytes read.
-		// Use buffer[:bytesRead] to work with only the actual number of bytes read
-		// in the current iteration, especially if the last chunk is smaller than 8 bytes.
-		fmt.Printf("read: %s\n", string(buffer[:bytesRead]))
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
